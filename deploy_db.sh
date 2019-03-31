a6ecd9082724ded1ab7114952658c7cfebc2d420
chulman <chlcjfals0122@gmail.com>
2019-03-31 22:45:17 +0900

    db deploy 분리.

--- deploy_db.sh
+++ deploy_db.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-# Absolute path
-PATH_NAME=$(pwd)
-echo $PATH_NAME
+####################################################################
+##  ./deploy_db.sh mysql
+####################################################################
 
-# MySQL5.6 run
-docker run -d -it --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=mysql -e MYSQL_PASSWORD=mysql -e MYSQL_DATABASE=kanban -e TZ=Asia/Seoul \
--v $PATH_NAME/volume/mysql/script/init.sql:/docker-entrypoint-initdb.d/init.sql \
--v $PATH_NAME/volume/mysql:/var/lib/mysql \
---health-cmd='mysqladmin -uroot -proot ping -h localhost' \
---health-interval=20s \
---health-timeout=3s \
---health-retries=5 \
-mysql:5.6
+show_usage()
+{
+	echo "usage: $0 mysql"
+}
+
+if [[ $1 -eq 'mysql' ]]; then
+    # run mysql
+    docker run -it \
+               -v "$PWD/mysql/script/init.sql":/docker-entrypoint-initdb.d/init.sql -v "$PWD/volume/mysql":/var/lib/mysql \
+               -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=mysql -e MYSQL_DATABASE=kanban -e TZ=Asia/Seoul \
+               -p 3306:3306 \
+               --name mysql \
+               --health-cmd "mysqladmin -uroot -proot ping -h localhost" --health-interval=20s --health-timeout=3s --health-retries=5 \
+               -d mysql:5.6
+else
+   show_usage
+   exit;
+fi
+
